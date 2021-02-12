65132cada98ead3fdf870790428a00104b454e57
nearnshaw <nearnshaw@decentraland.org>
2021-02-12 16:02:57 -0300

    new utils lib version

--- src/game.ts
+++ src/game.ts
@@ -1,26 +1,33 @@
-import utils from "../node_modules/decentraland-ecs-utils/index"
-import { Coin } from "./coin"
-import { Switchboard } from "./switchboard"
+import * as utils from '@dcl/ecs-scene-utils'
+import { Coin } from './coin'
+import { Switchboard } from './switchboard'
 
 // Base
 const base = new Entity()
-base.addComponent(new GLTFShape("models/baseLight.glb"))
+base.addComponent(new GLTFShape('models/baseLight.glb'))
 base.addComponent(new Transform({ scale: new Vector3(2, 1, 1) }))
 engine.addEntity(base)
 
 // Platform and rails
 const platforms = new Entity()
-platforms.addComponent(new GLTFShape("models/platforms.glb"))
+platforms.addComponent(new GLTFShape('models/platforms.glb'))
 engine.addEntity(platforms)
 
 // Coins
-const coinShape = new GLTFShape("models/starCoin.glb") // Includes the spinning animation
-let triggerBoxShape = new utils.TriggerBoxShape(new Vector3(1.5, 2.5, 1.5), new Vector3(0, 1, 0)) // Trigger shape for coin
-const coin = new Coin(coinShape, new Transform({ position: new Vector3(29, 6, 8) }), triggerBoxShape)
+const coinShape = new GLTFShape('models/starCoin.glb') // Includes the spinning animation
+let triggerBoxShape = new utils.TriggerBoxShape(
+  new Vector3(1.5, 2.5, 1.5),
+  new Vector3(0, 1, 0)
+) // Trigger shape for coin
+const coin = new Coin(
+  coinShape,
+  new Transform({ position: new Vector3(29, 6, 8) }),
+  triggerBoxShape
+)
 
 // Switchboard
-const buttonAShape = new GLTFShape("models/buttonA.glb")
-const buttonBShape = new GLTFShape("models/buttonB.glb")
+const buttonAShape = new GLTFShape('models/buttonA.glb')
+const buttonBShape = new GLTFShape('models/buttonB.glb')
 
 const buttonA = new Entity()
 buttonA.addComponent(buttonAShape)
@@ -31,11 +38,11 @@ buttonB.addComponent(buttonBShape)
 buttonB.addComponent(new Transform())
 
 const gears = new Entity()
-gears.addComponent(new GLTFShape("models/gears.glb"))
+gears.addComponent(new GLTFShape('models/gears.glb'))
 gears.addComponent(new Transform())
 
 const switchboard = new Switchboard(
-  new GLTFShape("models/switchboard.glb"),
+  new GLTFShape('models/switchboard.glb'),
   new Vector3(8, 3, 8),
   new Vector3(27, 3, 8),
   buttonA,
--- src/switchboard.ts
+++ src/switchboard.ts
@@ -1,15 +1,21 @@
-import utils from "../node_modules/decentraland-ecs-utils/index"
-import { TriggerBoxShape } from "../node_modules/decentraland-ecs-utils/triggers/triggerSystem"
+import * as utils from '@dcl/ecs-scene-utils'
 
 // Sound
 const switchSound = new Entity()
-switchSound.addComponent(new AudioSource(new AudioClip("sounds/switch.mp3")))
+switchSound.addComponent(new AudioSource(new AudioClip('sounds/switch.mp3')))
 switchSound.addComponent(new Transform())
 switchSound.getComponent(Transform).position = Camera.instance.position
 engine.addEntity(switchSound)
 
 export class Switchboard extends Entity {
-  constructor(model: GLTFShape, startPos: Vector3, endPos: Vector3, public buttonA: Entity, public buttonB: Entity, public gears: Entity) {
+  constructor(
+    model: GLTFShape,
+    startPos: Vector3,
+    endPos: Vector3,
+    public buttonA: Entity,
+    public buttonB: Entity,
+    public gears: Entity
+  ) {
     super()
     engine.addEntity(this)
     this.addComponent(model)
@@ -21,37 +27,59 @@ export class Switchboard extends Entity {
     gears.setParent(this)
 
     // Trigger configurations
-    const buttonTriggerA = new TriggerBoxShape(new Vector3(2.75, 2.75, 2.75), new Vector3(1.5, 2, 0))
-    const buttonTriggerB = new TriggerBoxShape(new Vector3(2.75, 2.75, 2.75), new Vector3(-1.5, 2, 0))
+    const buttonTriggerA = new utils.TriggerBoxShape(
+      new Vector3(2.75, 2.75, 2.75),
+      new Vector3(1.5, 2, 0)
+    )
+    const buttonTriggerB = new utils.TriggerBoxShape(
+      new Vector3(2.75, 2.75, 2.75),
+      new Vector3(-1.5, 2, 0)
+    )
 
     // Button triggers
     buttonA.addComponent(
-      new utils.TriggerComponent(buttonTriggerA, null, null, null, null, () => {
-        this.movePlatform(-0.12, 0, -180, endPos)
+      new utils.TriggerComponent(buttonTriggerA, {
+        onCameraEnter: () => {
+          this.movePlatform(-0.12, 0, -180, endPos)
+        },
       })
     )
 
     buttonB.addComponent(
-      new utils.TriggerComponent(buttonTriggerB, null, null, null, null, () => {
-        this.movePlatform(0, -0.12, 180, startPos)
+      new utils.TriggerComponent(buttonTriggerB, {
+        onCameraEnter: () => {
+          this.movePlatform(0, -0.12, 180, startPos)
+        },
       })
     )
   }
 
-  private movePlatform(buttonAPos: number, buttonBPos: number, rotationSpeed: number, targetPos: Vector3) {
+  private movePlatform(
+    buttonAPos: number,
+    buttonBPos: number,
+    rotationSpeed: number,
+    targetPos: Vector3
+  ) {
     switchSound.getComponent(AudioSource).playOnce()
     this.buttonA.getComponent(Transform).position.y = buttonAPos
     this.buttonB.getComponent(Transform).position.y = buttonBPos
     let currentPos = this.getComponent(Transform).position
-    this.gears.addComponentOrReplace(new utils.KeepRotatingComponent(Quaternion.Euler(0, 0, rotationSpeed)))
+    this.gears.addComponentOrReplace(
+      new utils.KeepRotatingComponent(Quaternion.Euler(0, 0, rotationSpeed))
+    )
     this.addComponentOrReplace(
-      new utils.MoveTransformComponent(currentPos, targetPos, Math.abs(targetPos.x - currentPos.x) * 0.25, () => {
-        // Reset buttons
-        switchSound.getComponent(AudioSource).playOnce()
-        this.buttonA.getComponent(Transform).position.y = 0
-        this.buttonB.getComponent(Transform).position.y = 0
-        this.gears.getComponent(utils.KeepRotatingComponent).stop()
-      })
+      new utils.MoveTransformComponent(
+        currentPos,
+        targetPos,
+        Math.abs(targetPos.x - currentPos.x) * 0.25,
+        () => {
+          // Reset buttons
+          switchSound.getComponent(AudioSource).playOnce()
+          this.buttonA.getComponent(Transform).position.y = 0
+          this.buttonB.getComponent(Transform).position.y = 0
+          this.gears.getComponent(utils.KeepRotatingComponent).stop()
+        }
+      )
     )
   }
 }
